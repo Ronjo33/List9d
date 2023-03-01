0d3532d16f7fcba818dc40ce00b52d70de44eb2b
Ronjo33 <zlatomir.stefanov480@edu1.pgzaimov.com>
2023-03-01 11:19:33 +0200

    Add project files.

---
+++ Fibuna4i/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibuna4i
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("koe pod red 4islo na fibuna4i iskash da vkarassh:");
+            byte numFib = byte.Parse(Console.ReadLine());
+            List<int> num = new List<int>();
+            num.Add(1);
+            num.Add(1);
+            
+            
+            for (int i = 2; i < numFib; i++)
+            {
+                num.Add(num[i-2]+ num[i-1] );
+            }
+            Console.WriteLine(string.Join(",",num));
+        }
+    }
+}
---
+++ list1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("vuvedi kolko 4isla iskash:");
+            int n = int.Parse(Console.ReadLine());
+            List<int> num = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Vuvedi cislo");
+                int item = int.Parse(Console.ReadLine());
+                num.Add(item);
+            }
+            ////v sreadta
+            //int newindex = n / 2;
+            //num.Insert(newindex, 6);
+            ////dobavqne  v kraq na min element 
+            //int minElement = num.Min();
+            //num.Add(minElement);
+
+            //da se iztrie nulite priqteli
+            //foreach (var cehervenokv in num) 
+            while(num.Contains(0))
+            {
+                num.Remove(0);
+            }
+            
+            for (int i = 0; i < num.Count; i++)
+            {
+                Console.WriteLine($"numbers[{i}] = {num[i]}");
+            }
+            Console.WriteLine("broq na elementite v spisuka e:" + num.Count);
+        }
+    }
+}
